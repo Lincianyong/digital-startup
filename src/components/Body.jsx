45f35c8f517ec58b799b19cd0908c9dd7eac0bc0
Lincianyong <110042648+Lincianyong@users.noreply.github.com>
2023-05-29 22:26:22 +0700

    navbar

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Body () {
+    return(
+        <section>
+            <div className="h-screen w-screen bg-cover">
+
+            </div>  
+        </section>
+    )
+}
+
+export default Body;
