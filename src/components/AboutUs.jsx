e56a6f5577314cfd7f75cfd60a259814a86043cf
Lincianyong <110042648+Lincianyong@users.noreply.github.com>
2023-05-31 15:48:05 +0700

    animation

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -16,19 +16,19 @@ export default function AboutUs () {
                         </p>
                     </div>
                     <div className="flex justify-around flex-wrap pb-20 text-xl font-semibold">
-                        <div className=" h-auto w-[320px] grid justify-items-center py-10 mt-8">
+                        <div className=" h-auto w-[320px] grid justify-items-center py-10 mt-8 hover:scale-110 duration-500">
                             <img src={ImageIndonesia} alt="Indonesia" className=" w-[15rem]"/>
                             <div className="pt-14">Available throughout Indonesia</div>
                         </div>
-                        <div className="h-auto w-[320px] grid justify-items-center  py-10">
+                        <div className="h-auto w-[320px] grid justify-items-center  py-10 hover:scale-110 duration-500">
                             <img src={Sovel} alt="Indonesia" className=" h-40 w-[11rem]" />
                             <div className="pt-8">Over 1,000 partners</div>
                         </div>
-                        <div className="h-auto w-[320px] grid justify-items-center  py-10">
+                        <div className="h-auto w-[320px] grid justify-items-center  py-10 hover:scale-110 duration-500">
                             <img src={People} alt="Indonesia" className=" h-40 w-[11rem]" />
                             <div className="pt-8">Over 100,000 users</div>
                         </div>
-                        <div className="h-auto w-[320px] grid justify-items-center  py-10">
+                        <div className="h-auto w-[320px] grid justify-items-center  py-10 hover:scale-110 duration-500">
                             <img src={Seven} alt="Indonesia" className=" h-40 w-[15rem]" />
                             <div className="pt-8">Over 7 years experiecne</div>
                         </div>
